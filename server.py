0bb8383515aa573cc9cc17e775fa8dd527788e9e
VTRIOPRATHAP\vtrio <prathap.kumar@vtrio.com>
2024-03-05 16:55:07 +0530

    pass project details from config file

--- server.py
+++ server.py
@@ -2,28 +2,29 @@ from flask import Flask, jsonify, request
 import vertexai
 from vertexai.language_models import TextGenerationModel
 import os
+import json
 from google.auth import default
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "config/service-account-key.json"
 
 
 app = Flask(__name__)
+
+# Read project_id and location from config file
+with open('config/config.json', 'r') as config_file:
+    config_data = json.load(config_file)
+    project = config_data.get('project_id')
+    location = config_data.get('location')
+
 credentials, project_id = default()
 
 
 def generateText(
-    project_id: str,
-    location: str,
+    prompt: str,
 ) -> str:
 
-    vertexai.init(project=project_id, location=location)
-
-    # Get query parameters from the request
-    request_data = request.get_json()
-    if not request_data or 'prompt' not in request_data:
-        return jsonify({'error': 'Invalid JSON data or missing "prompt" key'}), 400
-
-
+    vertexai.init(project=project, location=location)
+    
     # override these parameters as needed:
     parameters = {
         "temperature": 0.0,  # Temperature controls the degree of randomness in token selection -  0.0–1.0
@@ -32,7 +33,6 @@ def generateText(
         "top_k": 1,  # A top_k of 1 means the selected token is the most probable among all tokens. - 1–40
     }
 
-    prompt = str(request_data.get('prompt'))
     model = TextGenerationModel.from_pretrained("text-bison@002")
     response = model.predict(
         prompt,
@@ -44,7 +44,13 @@ def generateText(
     
 @app.route('/api/data', methods=['POST'])
 def getData():
-    data = generateText('prompt-to-text-ai-project', 'us-central1')
+    # Get query parameters from the request
+    request_data = request.get_json()
+    if not request_data or 'prompt' not in request_data:
+        return jsonify({'error': 'Invalid JSON data or missing "prompt" key'}), 400
+    prompt = str(request_data.get('prompt'))
+
+    data = generateText(prompt)
     return data
 
 if __name__ == '__main__':
